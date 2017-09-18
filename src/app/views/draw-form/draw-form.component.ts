96456693287c0adb60b582545e8168a2f1cd6afa
Evgeny Smolovich <ivgi84@gmail.com>
2017-09-18 23:36:25 +0300

    adding canvas

--- src/app/views/draw-form/draw-form.component.ts
+++ src/app/views/draw-form/draw-form.component.ts
@@ -1,3 +1,4 @@
+import { ElementRef, ViewChild } from '@angular/core';
 import { forEach } from '@angular/router/src/utils/collection';
 import { UploadType } from './models/upload-type';
 import { debug } from 'util';
@@ -9,20 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./draw-form.component.css']
 })
 export class DrawFormComponent{
-  
+
+  @ViewChild('canvas') canvas: ElementRef;
+  @ViewChild('graphicsImg') graphicsImg: ElementRef;
+  @ViewChild('overlayImg') overlayImg: ElementRef;
+    
   private graphics = new UploadType(1, 'Graphics',true, '');
   private overlay = new UploadType(2, 'Overlay',false, '');
   private uploadTypes = [this.graphics, this.overlay];
   private selectedType = this.uploadTypes[0];
 
+  private ctx: CanvasRenderingContext2D;
+  private graphicsImgElem: HTMLImageElement;
+  private overlayImgElem: HTMLImageElement;
+  
+  ngAfterViewChecked(){
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+  }
+
   onFileSelect(file){
     let reader = new FileReader();
     reader.onload = (e: any) => {
-  
       this.selectedType.imgSrc = e.target.result;
+       setTimeout(()=>{
+         debugger
+        if(this.selectedType.value == 1)
+        this.graphicsImgElem = this.graphicsImg.nativeElement;
+
+      if(this.selectedType.value == 2)
+        this.overlayImgElem = this.overlayImg.nativeElement;
+       },1000);
     }
     reader.readAsDataURL(file[0]);  
-    debugger;
   }
 
   onUploadTypeSelection(ind){
@@ -31,6 +50,16 @@ export class DrawFormComponent{
     });
     this.selectedType = this.uploadTypes[ind];
   }
+  
+  clear() {
+    this.ctx.clearRect(0, 0, 800, 800);
+}
+
+  render() {
+    this.clear();
+    this.ctx.drawImage(this.graphicsImgElem, 0, 0);
+    this.ctx.drawImage(this.overlayImgElem, 0, 0);
+}
 
 
 }
