f04e80e948b5b68855b51d2ea05de423e8335f8c
Evgeny Smolovich <evgenysmo@etoro.com>
2017-11-15 18:35:29 +0200

    color picker in work

--- src/app/views/draw-form/tools/color-picker/lgos-color-picker.component.ts
+++ src/app/views/draw-form/tools/color-picker/lgos-color-picker.component.ts
@@ -1,32 +1,27 @@
 import { Component, OnInit, Input, ViewChild, AfterViewInit, ElementRef} from '@angular/core';
 import { Element } from '../../models/element';
 import { debug } from 'util';
+import { UserInput } from 'app/views/draw-form/models/user-input';
 
 @Component({
   selector: 'lgos-color-picker',
   templateUrl: './lgos-color-picker.component.html',
   styleUrls: ['./lgos-color-picker.component.css']
 })
-export class LgosColorPickerComponent implements OnInit {
+export class LgosColorPickerComponent {
 
   @ViewChild('colorPalete') colorPalete: ElementRef;
-  @Input() elm:Element;
+  @Input() elm:UserInput;
+
   isEnabled = false;
   ctx: CanvasRenderingContext2D;
+  pixelColor = '';
+  presetColors = [
+      '#fff',
+      '#000'
+  ]
+
   private isFirstInit = true;
-
-  constructor(){}
-
-  presets = {
-    colors:[]
-  }
-
-  ngOnInit() {
-  }
-
-  ngAfterViewInit(){
-    
-  }
 
   toggle(){
     this.isEnabled = !this.isEnabled;
@@ -36,11 +31,9 @@ export class LgosColorPickerComponent implements OnInit {
       },10);
       this.isFirstInit = false;
     }
-      
   }
 
   setCanvas(){
-    debugger;
     this.ctx = this.colorPalete.nativeElement.getContext('2d');
     let image = new Image();
     image.src='assets/img/colorwheel.png';
@@ -48,16 +41,19 @@ export class LgosColorPickerComponent implements OnInit {
       this.ctx.drawImage(image,0,0, image.width, image.height);
     }
   }
-  onMouseMove(e){
-    debugger;
-    let canvasOffset = this.colorPalete.nativeElement;
+  getColor(e){
+    let canvasElm = this.colorPalete.nativeElement;
+    let imageData = this.ctx.getImageData(e.offsetX, e.offsetY,1,1);
+    let pixel = imageData.data;
+    this.pixelColor = 'rgb('+pixel[0]+','+pixel[1]+','+pixel[2]+')';
   }
-
-  addColorToPreset(){
-
+  setColor(data){
+    if(!data)
+      this.elm.addStyle({'color':this.pixelColor});
+    else{
+      let colorObj = { color:data };
+      this.elm.addStyle(colorObj);
+    }
+     
   }
-  pickColor(){
-    
-  }
-
 }
